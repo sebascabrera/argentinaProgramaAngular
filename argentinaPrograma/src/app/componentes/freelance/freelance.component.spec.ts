dc3aa30ab473af3db38dd8b2d33a9770cc8b2b6c
Sebastian <sebasfedele@gmail.com>
2022-05-09 22:45:48 -0300

    estructura de pagina

---
+++ argentinaPrograma/src/app/componentes/freelance/freelance.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FreelanceComponent } from './freelance.component';
+
+describe('FreelanceComponent', () => {
+  let component: FreelanceComponent;
+  let fixture: ComponentFixture<FreelanceComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FreelanceComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FreelanceComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
